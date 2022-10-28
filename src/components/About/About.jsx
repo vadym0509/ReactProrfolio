db44998168b3576c6813365f0bb66cdcb634156c
vadymrass <vadymrass0509@gmail.com>
2022-10-27 23:23:18 -0700

    added navbar with bootstrap

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,6 +7,9 @@ function About() {
             <h1 className='about__title'>About</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum unde ex quae sapiente delectus dolore, similique quos est reiciendis consequatur obcaecati laboriosam, nostrum fugiat nobis, inventore sequi nemo hic earum?</p>
             </div>
+            <div className='about__div2'>
+                <img src='' alt='' />
+            </div>
            
 
         </section>
--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,29 +1,41 @@
 import './NavBar.scss';
 import { Link } from "react-router-dom";
 import home from '../../assets/icons/home-icon.svg';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function NavBar() {
-    return (
-      <section className="nav">
-        <ul className="nav__list">
-          <Link to="/">
-            <li>Home</li>
-            
-          </Link>
-          <Link to="/about">
-          <li>About</li>
-          </Link>
-          <Link to="/projects">
-            <img src={home} alt="game button" className="nav__img" />
-          </Link>
-          <Link to="/hackathonsProjects">
-            <img src={home} alt="recycling button" className="nav__img" />
-          </Link>
-          <Link to="/resume">
-            <img src={home} alt="recycling button" className="nav__img" />
-          </Link>
-        </ul>
-      </section>
+  return (
+    <>
+    <Navbar className="nav" bg="light" expand="lg">
+    <Container fluid>
+    <Navbar.Brand href="#">KR</Navbar.Brand>
+   
+    <Nav
+    className="me-auto my-2 my-lg-0"
+    style={{ zindex: '10' }}
+    navbarScroll
+    >
+    <Nav.Link href="#action1">Home</Nav.Link>
+    <Nav.Link href="#action2">About</Nav.Link>
+    <NavDropdown title="Projects" id="navbarScrollingDropdown">
+    <NavDropdown.Item href="#action3">Deployed</NavDropdown.Item>
+    <NavDropdown.Item href="#action4">
+    Hackathon Projects
+    </NavDropdown.Item>
+    <NavDropdown.Item href="#action5">
+    General Projects
+    </NavDropdown.Item>
+    </NavDropdown>
+    
+    </Nav>
+    
+    </Container>
+    </Navbar>
+    
+    </>
     );
   }
   export default NavBar;
