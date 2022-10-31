a52ebb009c9467dfce29cbb64b6f665c3a74dfeb
vadymrass <vadymrass0509@gmail.com>
2022-10-31 00:15:29 -0700

    tried a second type of navbar

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,41 +1,21 @@
+import * as React from 'react';
 import './NavBar.scss';
-import { Link } from "react-router-dom";
-import home from '../../assets/icons/home-icon.svg';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
 
-function NavBar() {
+export default function NavBar() {
+  
   return (
-    <>
-    <Navbar className="nav" bg="light" expand="lg">
-    <Container fluid>
-    <Navbar.Brand href="#">KR</Navbar.Brand>
-   
-    <Nav
-    className="me-auto my-2 my-lg-0"
-    style={{ zindex: '10' }}
-    navbarScroll
-    >
-    <Nav.Link href="#action1">Home</Nav.Link>
-    <Nav.Link href="#action2">About</Nav.Link>
-    <NavDropdown title="Projects" id="navbarScrollingDropdown">
-    <NavDropdown.Item href="#action3">Deployed</NavDropdown.Item>
-    <NavDropdown.Item href="#action4">
-    Hackathon Projects
-    </NavDropdown.Item>
-    <NavDropdown.Item href="#action5">
-    General Projects
-    </NavDropdown.Item>
-    </NavDropdown>
-    
-    </Nav>
-    
-    </Container>
-    </Navbar>
-    
-    </>
-    );
-  }
-  export default NavBar;
+    <section className='navBar'>
+    <React.Fragment>
+      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+       <Avatar src="../../assets/images/kayle.jpg" alt="kayle robson"/>
+       
+        <Typography sx={{ minWidth: 100 }}>About</Typography>
+        <Typography sx={{ minWidth: 100 }}>Projects</Typography>
+      </Box>
+    </React.Fragment>
+    </section>
+  );
+}
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,12 +1,12 @@
 import Banner from "../components/Banner/Banner";
+import Nav from "../components/Nav/Nav";
 import NavBar from "../components/Navbar/NavBar";
 
 function LandingPage() {
   return (
     <section className="landing">
-        <NavBar />
+      <NavBar />
       <Banner />
-      
     </section>
   );
 }
