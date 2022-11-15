1b40bbf3eea0e1ac3f83bfbbef502fb610a75bd3
vadymrass <vadymrass0509@gmail.com>
2022-11-14 22:04:38 -0800

    worked on navbar on home page

--- src/components/BottomNav/BottomNav.jsx
+++ src/components/BottomNav/BottomNav.jsx
@@ -1,21 +1,24 @@
 import "./BottomNav.scss";
 import { NavLink } from "react-router-dom";
 import home from "../../assets/icons/home-icon.svg";
+import info from "../../assets/icons/info-icon.png";
+import project from "../../assets/icons/project-icon.png";
+
 
 function BottomNav() {
   return (
     <section className="bottomNav">
       <NavLink className="bottomNav__link" to="/">
         <img src={home} className="bottomNav__logo" alt="Brainflix logo" />
-        <p>Home</p>
+        <p className="bottomNav__p">Home</p>
       </NavLink>
       <NavLink className="bottomNav__link" to="/about">
-        <img src={home} className="bottomNav__logo" alt="Brainflix logo" />
-        <p>About</p>
+        <img src={info} className="bottomNav__logo" alt="Brainflix logo" />
+        <p className="bottomNav__p">About</p>
       </NavLink>
       <NavLink className="bottomNav__link" to="/projects">
-        <img src={home} className="bottomNav__logo" alt="Brainflix logo" />
-        <p>Projects</p>
+        <img src={project} className="bottomNav__logo" alt="Brainflix logo" />
+        <p className="bottomNav__p">Projects</p>
       </NavLink>
     </section>
   );
