0763a9eea72a7fcd7fb689c306af3f0952e73c32
vadymrass <vadymrass0509@gmail.com>
2022-12-11 10:18:57 -0800

    added a different color and box shadow to the navbar

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -13,16 +13,16 @@ function About() {
         <h1 className='about__title'>I'm <span className="about__title--change">Kayle Robson</span>.  I am fervid about coding and hackathons!</h1>
         
         <p className='about__p'>I am a team player and I love working with people and making the best possible 
-        outcome for a project.  I have participated in 2 worldwide and three within 
-        my Brainstation Bootcamp.   The 2 international hackathons required creating a viable idea within specific time 
+        outcome for a project.  I have participated in 2 worldwide hackathons and three within 
+        the Brainstation Web Development bootcamp.   The 2 international hackathons required creating a viable idea within specific time 
         constraints; my teams completed this successfully, overcoming barriers related to language and time zone differences.   
         The graphic designer I worked with in the Belarus hackathon joined my team in the India Hackathon and also I used her 
         graphics in my final bootcamp project.  </p>
         <p className='about__p'>The three hackathons at Brainstation all came with their own challenges.  The API 
         hackathons brought the excitement of learning about APIs, using Git, Agile methodology, and designing a full stack web 
-        application within 5 hours!  The industry hackathon with Telus Digital was a paired hackathon with the UI/UX Designers.   
+        application within 5 hours!  The industry hackathon with Telus Digital, which we won, was a paired hackathon with the UI/UX Designers.   
         We created reusable code for the components and the application was built with the idea of connecting it to a backend.</p>   
-        <p className='about__p'>I am currently working at Brainstation as a Teaching Assistant for the current cohort, October to December 2022.  I mentor 
+        <p className='about__p'>I am currently working at Brainstation as a Teaching Assistant for the current Web Development cohort, October to December 2022.  I mentor 
         and help teach 36 students and provide feedback to students to ensure they comprehend the material, can apply it to real world 
         coding problems, and follow best practices in the Web Development industry.  I plan and deliver supplementary content to enrich 
         the learning experience of students.</p>
