632743ed1b3260cf9d64eba160fb79e7154b26a3
vadymrass <vadymrass0509@gmail.com>
2023-01-28 21:33:20 -0800

    changed to bottom nav

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,12 +1,14 @@
 import About from "../components/About/About";
-import TopNav from "../components/TopNav/TopNav";
+//import TopNav from "../components/TopNav/TopNav";
 import Footer from "../components/Footer/Footer";
+import BottomNav from '../components/BottomNav/BottomNav';
 
 function AboutPage() {
     return(
         <>
-        <TopNav />
+        {/* <TopNav /> */}
         <About />
+        <BottomNav/>
        <Footer/>
         </>
     );
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -4,6 +4,8 @@ import ProjectData from "../data/projects.json";
 import TopNav from "../components/TopNav/TopNav";
 import HackData from "../data/hackProjects.json";
 import Footer from "../components/Footer/Footer";
+import BottomNav from '../components/BottomNav/BottomNav';
+
 
 function ProjectPage() {
   //create a state with project data
@@ -14,9 +16,10 @@ function ProjectPage() {
   
   return (
     <>
-    <TopNav />
+    {/* <TopNav /> */}
     <ProjectList projects={projects} />
     <ProjectList projects={hackProjects} />
+    <BottomNav/>
     <Footer/>
     </>
     );
