705bd1c08b29e585e130598e2cd80dd51ab647e0
vadymrass <vadymrass0509@gmail.com>
2023-02-21 21:47:05 -0800

    added a loading circle but needs work and added another project

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,19 +1,36 @@
-import { useState } from "react";
 import ProjectList from "../components/ProjectList/ProjectList";
 import ProjectData from "../data/projects.json";
 import TopNav from "../components/TopNav/TopNav";
 import HackData from "../data/hackProjects.json";
 import Footer from "../components/Footer/Footer";
-//import BottomNav from '../components/BottomNav/BottomNav';
-
+import { useState, useEffect } from "react";
 
 function ProjectPage() {
   //create a state with project data
   const [projects] = useState(ProjectData);
   const [hackProjects] = useState(HackData);
-  console.log(projects)
-  console.log(hackProjects)
-  
+
+    /*
+    *Loading when pressed Links back to Homepage
+    *start State in true so that we can
+    *toggle between loading
+    */
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(() => {
+        // Loading function to load data or
+        // fake it using setTimeout;
+        const loadData = async () => {
+            // wait for 2 secs if there is no wait
+            await new Promise((resolved) => setTimeout(resolved, 2000));
+            // set the toggle loading state
+            setLoading((loading) => !loading);
+        };
+        loadData();
+    }, []);
+    if (loading) {
+        return <div className="loader"></div>;
+    } else {
   return (
     <>
     <TopNav />
@@ -24,6 +41,7 @@ function ProjectPage() {
     </>
     );
   }
+}
   
   export default ProjectPage;
   
