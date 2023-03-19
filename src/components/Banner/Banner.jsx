3ee932b4d53195fa70919f19ee7fe0e4beec97de
vadymrass <vadymrass0509@gmail.com>
2023-03-19 07:13:53 -0700

    fixed banner page

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,48 +1,16 @@
 import { ReactP5Wrapper } from "react-p5-wrapper";
-//import Typed from 'typed.js';
 import sketch from "../../sketch";
-//import { useEffect, useRef } from "react";
 import './Banner.scss';
 
 function Banner() {
-  //   // Create reference to store the DOM element containing the animation
-	// const el = useRef(null);
-  //   // Create reference to store the Typed instance itself
-  //   const typed = useRef(null);
   
-  //   useEffect(() => {
-    //   const options = {
-    //       strings: [
-    //         "Hello World",
-    //         "I am a Full Stack Developer",
-    //         "I am keen to learn",
-    //         "I am fervid about coding",
-    //         "I debug code like a jed",
-    //         "I debug code like a mast",
-    //         "I debug code like a junior developer!"
-    //     ],
-    //     typeSpeed: 150,
-    //     backSpeed: 100,
-    //   };
-      
-    //   // elRef refers to the <span> rendered below
-    //   typed.current = new Typed(el.current, options);
-      
-    //   return () => {
-    //     // Make sure to destroy Typed instance during cleanup
-    //     // to prevent memory leaks
-    //     typed.current.destroy();
-    //   }
-    // }, [])
     
     return(
         <section className="banner">
             <div className="banner__sketch">
                 <ReactP5Wrapper sketch={sketch} />
             </div>
-            {/* <div className="banner__about">
-            <span style={{ whiteSpace: 'pre' }} ref={el} />
-            </div> */}
+           
         </section>
         );
     };
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -3,45 +3,23 @@ import ProjectData from "../data/projects.json";
 import TopNav from "../components/TopNav/TopNav";
 import HackData from "../data/hackProjects.json";
 import Footer from "../components/Footer/Footer";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function ProjectPage() {
   //create a state with project data
   const [projects] = useState(ProjectData);
   const [hackProjects] = useState(HackData);
 
-    /*
-    *Loading when pressed Links back to Homepage
-    *start State in true so that we can
-    *toggle between loading
-    */
-    const [loading, setLoading] = useState(true);
-    
-    useEffect(() => {
-        // Loading function to load data or
-        // fake it using setTimeout;
-        const loadData = async () => {
-            // wait for 2 secs if there is no wait
-            await new Promise((resolved) => setTimeout(resolved, 2000));
-            // set the toggle loading state
-            setLoading((loading) => !loading);
-        };
-        loadData();
-    }, []);
-    if (loading) {
-        return <div className="loader"></div>;
-    } else {
   return (
     <>
     <TopNav />
     <ProjectList projects={projects} />
     <ProjectList projects={hackProjects} />
-    {/* <BottomNav/> */}
     <Footer/>
     </>
     );
   }
-}
+//}
   
   export default ProjectPage;
   
