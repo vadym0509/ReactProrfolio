d282ca829c16b20ea3f64fe33d72de895178211a
vadymrass <vadymrass0509@gmail.com>
2023-03-22 22:26:32 -0700

    fixed some info in the json and styled the font-size smaller within the cards

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,20 +1,17 @@
 import ProjectList from "../components/ProjectList/ProjectList";
 import ProjectData from "../data/projects.json";
 import TopNav from "../components/TopNav/TopNav";
-import HackData from "../data/hackProjects.json";
 import Footer from "../components/Footer/Footer";
 import { useState } from "react";
 
 function ProjectPage() {
   //create a state with project data
   const [projects] = useState(ProjectData);
-  const [hackProjects] = useState(HackData);
 
   return (
     <>
     <TopNav />
     <ProjectList projects={projects} />
-    <ProjectList projects={hackProjects} />
     <Footer/>
     </>
     );
